a647f845c2bf060768907c654af751759309e7ef
SHergibo <sachau888@hotmail.com>
2022-06-10 15:09:08 +0200

    feat: Added routing in App.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FC } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './Pages/HomePage';
+import SignInSignUp from './Pages/SignInSignUp';
+import Admin from './Pages/Admin';
+import Page404 from './Pages/Page404';
 
-function App() {
+const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/login" element={<SignInSignUp />} />
+        <Route path="*" element={<Page404 />} />
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
