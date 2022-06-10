e60bf1dde4d4cd312b174a554dfa0393be993edc
SHergibo <sachau888@hotmail.com>
2022-06-10 16:18:51 +0200

    feat: Created environment.config.js in config folder.

---
+++ config/environment.config.ts
@@ -0,0 +1,4 @@
+const { REACT_APP_API_DOMAIN, REACT_APP_API_VERSION } = process.env;
+
+export const apiDomain: string | undefined = REACT_APP_API_DOMAIN;
+export const apiVersion: string | undefined = REACT_APP_API_VERSION;
