30606788dba00dc757a22cb697594e40b0369df3
SHergibo <sachau888@hotmail.com>
2022-06-11 17:03:30 +0200

    feat: Added createContext in App component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import { FC, useState, useEffect, useCallback } from 'react';
-import axios from 'axios';
-import { apiDomain, apiVersion } from './../config/environment.config';
+import { FC, useState, useEffect, createContext } from 'react';
+import { useAxiosFetch } from './Hooks/useAxiosHooks';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 import SignInSignUp from './Pages/SignInSignUp';
@@ -8,17 +7,28 @@ import Admin from './Pages/Admin';
 import Page404 from './Pages/Page404';
 import { IGeneralInfo } from './interfaces';
 
+export const generalInfoContext = createContext<IGeneralInfo | null>(null);
+
 const App: FC = () => {
-  const [generalInfo, setGeneralInfo] = useState<IGeneralInfo | object>({});
+  const [generalInfo, setGeneralInfo] = useState<IGeneralInfo | null>(null);
+  const { data, done } = useAxiosFetch<IGeneralInfo>('/infos');
+
+  useEffect(() => {
+    if (done) {
+      setGeneralInfo(data);
+    }
+  }, [data, done]);
 
   return (
     <>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/login" element={<SignInSignUp />} />
-        <Route path="*" element={<Page404 />} />
-      </Routes>
+      <generalInfoContext.Provider value={generalInfo}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/login" element={<SignInSignUp />} />
+          <Route path="*" element={<Page404 />} />
+        </Routes>
+      </generalInfoContext.Provider>
     </>
   );
 };
