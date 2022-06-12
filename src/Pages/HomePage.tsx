c5a4f0f6fa6ea036816f2c20ef33539d59e9bbf2
SHergibo <sachau888@hotmail.com>
2022-06-12 18:20:33 +0200

    feat: Added About, Contact, Resume and Portfolio component and a loader in HomePage component.

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,7 +1,39 @@
-import { FC } from 'react';
+import { useState, FC, ReactElement } from 'react';
+import About from '../Components/About';
+import Contact from '../Components/Contact';
+import Portfolio from '../Components/Portfolio';
+import Resume from '../Components/Resume';
+import { IGeneralInfo } from './../interfaces';
 
-const HomagePage: FC = () => {
-  return <div>HomePage</div>;
+interface ILoaded {
+  isLoaded: boolean;
+  error: boolean;
+}
+
+export interface IFetchLoaded {
+  info: ILoaded;
+  portfolio: ILoaded;
+  resume: ILoaded;
+}
+
+const HomagePage: FC = (): ReactElement => {
+  const [generalInfo, setGeneralInfo] = useState<IGeneralInfo | null>(null);
+  const [fetchLoaded, setFetchLoaded] = useState<IFetchLoaded>({
+    info: { isLoaded: false, error: false },
+    portfolio: { isLoaded: false, error: false },
+    resume: { isLoaded: false, error: false }
+  });
+
+  return (
+    <>
+      {!fetchLoaded.info.isLoaded && !fetchLoaded.portfolio.isLoaded && !fetchLoaded.resume.isLoaded && <p>Is Loading ...</p>}
+      <h1>Homepage</h1>
+      <About setGeneralInfo={setGeneralInfo} fetchLoaded={fetchLoaded} setFetchLoaded={setFetchLoaded} />
+      <Portfolio fetchLoaded={fetchLoaded} setFetchLoaded={setFetchLoaded} />
+      <Resume fetchLoaded={fetchLoaded} setFetchLoaded={setFetchLoaded} />
+      <Contact generalInfo={generalInfo} />
+    </>
+  );
 };
 
 export default HomagePage;
