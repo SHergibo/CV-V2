a4ec0fb41774e56c9ca40080a41c7920bbd71533
SHergibo <sachau888@hotmail.com>
2022-06-12 18:18:10 +0200

    fix: Deleted fetching of generalInfo and createContext in App component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import { FC, useState, useEffect, createContext } from 'react';
-import { useAxiosFetch } from './Hooks/useAxiosHooks';
+import { FC, ReactElement } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 import SignInSignUp from './Pages/SignInSignUp';
 import Admin from './Pages/Admin';
 import Page404 from './Pages/Page404';
-import { IGeneralInfo } from './interfaces';
 
-export const generalInfoContext = createContext<IGeneralInfo | null>(null);
-
-const App: FC = () => {
-  const [generalInfo, setGeneralInfo] = useState<IGeneralInfo | null>(null);
-  const { data, done } = useAxiosFetch<IGeneralInfo>('/infos');
-
-  useEffect(() => {
-    if (done) {
-      setGeneralInfo(data);
-    }
-  }, [data, done]);
-
+const App: FC = (): ReactElement => {
   return (
-    <>
-      <generalInfoContext.Provider value={generalInfo}>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/login" element={<SignInSignUp />} />
-          <Route path="*" element={<Page404 />} />
-        </Routes>
-      </generalInfoContext.Provider>
-    </>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/admin" element={<Admin />} />
+      <Route path="/login" element={<SignInSignUp />} />
+      <Route path="*" element={<Page404 />} />
+    </Routes>
   );
 };
 
