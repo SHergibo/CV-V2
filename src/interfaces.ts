070aa2bd17116a631e4dccba70fcce6ca5503f98
SHergibo <sachau888@hotmail.com>
2022-06-12 18:21:25 +0200

    feat: Added interfaces for ITechnoUsed, IProject, IPortfolio, IEducExpResume, ISkillResume.

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -13,7 +13,7 @@ interface IProfTitle {
   svgIconProfTitle: string;
 }
 
-interface IprofPic {
+interface IImage {
   id: string;
   fileName: string;
   alt: string;
@@ -33,7 +33,48 @@ export interface IGeneralInfo {
   birthdate: string;
   licence: string;
   professionTitles: Array<IProfTitle>;
-  profilePic: IprofPic;
+  profilePic: IImage;
   hasPortfolio: boolean;
   hasResume: boolean;
+}
+
+interface ITechnoUsed {
+  value: string;
+  label: string;
+}
+
+interface IProject {
+  _id: string;
+  urlWeb: string;
+  urlGithub: string;
+  projectName: string;
+  pageIndex: string;
+  description: string;
+  technoUsedFront: Array<ITechnoUsed>;
+  technoUsedBack: Array<ITechnoUsed>;
+  images: Array<IImage>;
+}
+
+export interface IPortfolio {
+  arrayData: Array<IProject>;
+  totalData: number;
+}
+
+export interface IEducExpResume {
+  dateEnd: string;
+  dateStart: string;
+  educExpe: string;
+  placeEducExpe: string;
+  titleEducExpe: string;
+  _id: string;
+}
+
+export interface ISkillResume {
+  fontAwesomeIcon: {
+    [key: string]: IFontAwesomeIncon;
+  };
+  nameSkill: string;
+  skillCategory: string;
+  svgIcon: string;
+  _id: string;
 }
