32772aadf91f520d1ca0aeeaa69ed7e4455c6038
SHergibo <sachau888@hotmail.com>
2022-06-12 18:19:47 +0200

    feat: Updated useAxiosHook component.

--- src/Hooks/useAxiosHooks.tsx
+++ src/Hooks/useAxiosHooks.tsx
@@ -1,27 +1,26 @@
 import { useState, useEffect } from 'react';
-import axios, { AxiosResponse } from 'axios';
-import { apiDomain, apiVersion } from './../config/environment.config';
+import axios, { AxiosResponse, AxiosRequestConfig, AxiosError } from 'axios';
 
-export function useAxiosFetch<payload>(url: string): { data: payload | null; done: boolean } {
-  const [data, setData] = useState<payload | null>(null);
-  const [done, setDone] = useState<boolean>(false);
+export function useAxios<payload>(axiosParams: AxiosRequestConfig) {
+  const [response, setResponse] = useState<payload | null>(null);
+  const [error, setError] = useState<AxiosError | null>();
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const fetchData = async (params: AxiosRequestConfig) => {
+    try {
+      const result: AxiosResponse = await axios.request<payload>(params);
+      setResponse(result.data);
+    } catch (error: any) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    const axiosFetchData = async (url: string) => {
-      try {
-        await axios.get<payload>(`${apiDomain}/api/${apiVersion}${url}`).then((response: AxiosResponse) => {
-          setData(response.data);
-          setDone(true);
-        });
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    axiosFetchData(url);
-  }, [url]);
+    fetchData(axiosParams);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-  return {
-    data,
-    done
-  };
+  return { response, error, loading };
 }
