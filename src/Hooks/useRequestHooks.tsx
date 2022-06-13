2cc6ad7297169b30780ed02f47c518a33b1d2cab
SHergibo <sachau888@hotmail.com>
2022-06-13 17:36:35 +0200

    feat: Added loading variable to check if data is loaded in useRequestHook.

--- src/Hooks/useRequestHooks.tsx
+++ src/Hooks/useRequestHooks.tsx
@@ -4,14 +4,19 @@ import axios, { AxiosResponse, AxiosRequestConfig, AxiosError } from 'axios';
 interface Return<Data, Error> extends Pick<SWRResponse<AxiosResponse<Data>, AxiosError<Error>>, 'error'> {
   data: Data | undefined;
   response: AxiosResponse<Data> | undefined;
+  loading: boolean;
 }
 
 export function useRequest<Data = unknown, Error = unknown>(request: AxiosRequestConfig): Return<Data, Error> {
   const { data: response, error } = useSWR<AxiosResponse<Data>, AxiosError<Error>>(request && JSON.stringify(request), () => axios.request<Data>(request!));
+  let loading: boolean = true;
+
+  if (response && !error) loading = false;
 
   return {
     data: response && response.data,
     response,
+    loading,
     error
   };
 }
