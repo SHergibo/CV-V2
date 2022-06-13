2df5a7c1d1e9e1c889bec791a2327908223ab9b3
SHergibo <sachau888@hotmail.com>
2022-06-13 15:26:30 +0200

    feat: Used useRequestHook instead of useAxios hook in Portfolio component.

--- src/Components/Portfolio.tsx
+++ src/Components/Portfolio.tsx
@@ -1,40 +1,40 @@
 import { FC, ReactElement, Dispatch, SetStateAction, useEffect, useState } from 'react';
-import { useAxios } from '../Hooks/useAxiosHooks';
+import { useRequest } from '../Hooks/useRequestHooks';
 import { apiDomain, apiVersion } from '../config/environment.config';
 import { IPortfolio } from '../interfaces';
 import { IFetchLoaded } from '../Pages/HomePage';
 
 export interface IPortfolioProps {
-  fetchLoaded: IFetchLoaded;
   setFetchLoaded: Dispatch<SetStateAction<IFetchLoaded>>;
 }
 
-const Portfolio: FC<IPortfolioProps> = ({ fetchLoaded, setFetchLoaded }): ReactElement => {
+const Portfolio: FC<IPortfolioProps> = ({ setFetchLoaded }): ReactElement => {
   const [pageIndex, setPageIndex] = useState<number>(1);
-  const { response, loading, error } = useAxios<IPortfolio>({
+
+  const { data, error } = useRequest<IPortfolio>({
     method: 'get',
     url: `${apiDomain}/api/${apiVersion}/projects/pagination?page=${pageIndex - 1}`
   });
 
   useEffect(() => {
-    if (!loading && !error) {
-      setFetchLoaded({
-        ...fetchLoaded,
+    if (data && !error) {
+      setFetchLoaded((prevState) => ({
+        ...prevState,
         portfolio: { isLoaded: true, error: false }
-      });
+      }));
     }
     if (error) {
-      setFetchLoaded({
-        ...fetchLoaded,
+      setFetchLoaded((prevState) => ({
+        ...prevState,
         portfolio: { isLoaded: false, error: true }
-      });
+      }));
     }
-  }, [loading, error, fetchLoaded, setFetchLoaded]);
+  }, [data, error, setFetchLoaded]);
 
   return (
     <div>
       <h3>Portfolio</h3>
-      <p>{response?.arrayData[0]._id}</p>
+      <p>{data?.arrayData[0]._id}</p>
     </div>
   );
 };
