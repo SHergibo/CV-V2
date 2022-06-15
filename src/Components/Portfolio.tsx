42691340468dd60ab81335132a6bbcacf233906e
SHergibo <sachau888@hotmail.com>
2022-06-15 14:12:27 +0200

    fix: Deleted useless typing in Portfolio component.

--- src/Components/Portfolio.tsx
+++ src/Components/Portfolio.tsx
@@ -1,16 +1,16 @@
-import { FC, ReactElement, Dispatch, SetStateAction, useState } from 'react';
+import { Dispatch, SetStateAction, useState } from 'react';
 import { useRequest } from '../Hooks/useRequestHooks';
 import useIsLoaded from '../Hooks/useIsLoadedHook';
 import { apiDomain, apiVersion } from '../config/environment.config';
 import { IPortfolio } from '../interfaces';
 import { IFetchLoaded } from '../Pages/HomePage';
 
-export interface IPortfolioProps {
+interface IPortfolioProps {
   setFetchLoaded: Dispatch<SetStateAction<IFetchLoaded>>;
 }
 
-const Portfolio: FC<IPortfolioProps> = ({ setFetchLoaded }): ReactElement => {
-  const [pageIndex, setPageIndex] = useState<number>(1);
+const Portfolio = ({ setFetchLoaded }: IPortfolioProps) => {
+  const [pageIndex, setPageIndex] = useState(1);
 
   const { data, loading, error } = useRequest<IPortfolio>({
     method: 'get',
