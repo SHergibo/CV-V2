8649fde392e0d6a44d5842534baaf611c5317042
SHergibo <sachau888@hotmail.com>
2022-06-16 15:21:58 +0200

    feat: Added new colors in Theme.ts.

--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -5,6 +5,11 @@ declare module 'styled-components' {
   export interface DefaultTheme {
     colors: {
       mainBlue: string;
+      blue: string;
+      white: string;
+      greyOne: string;
+      greyThree: string;
+      greyBorder: string;
     };
     font: {
       principalFont: string;
@@ -14,7 +19,12 @@ declare module 'styled-components' {
 
 const theme: DefaultTheme = {
   colors: {
-    mainBlue: 'hsl(211, 80%, 10%)'
+    mainBlue: 'hsl(211, 80%, 10%)',
+    blue: 'hsl(208, 100%, 43%)',
+    white: '#ffffff',
+    greyOne: 'hsla(0, 0%, 100%, 0.7)',
+    greyThree: 'hsla(0, 0%, 100%, 0.62)',
+    greyBorder: 'hsla(0, 0%, 100%, 0.3)'
   },
   font: {
     principalFont: '"Poppins", sans-serif'
