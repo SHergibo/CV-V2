276f2510c51861840f76e91b903fcfb0da414cac
SHergibo <sachau888@hotmail.com>
2022-06-17 15:48:23 +0200

    feat: Created Nav and Nav.styled files in Nav folder.

---
+++ src/Components/Nav/Nav.styles.ts
@@ -0,0 +1,138 @@
+import styled from 'styled-components';
+import { Wrapper } from '../../styles/components/Layout.styled';
+
+interface IMainMenuProp {
+  mainMenufixed: boolean;
+}
+
+export const MainMenu = styled.div<IMainMenuProp>`
+  background-color: ${({ theme }) => theme.colors.mainBlue};
+  border-bottom: 0.5px solid ${({ theme }) => theme.colors.greyBorder};
+  position: ${({ mainMenufixed }) => (mainMenufixed ? 'fixed' : 'absolute')};
+  top: ${({ mainMenufixed }) => (mainMenufixed ? '0' : 'unset')};
+  left: 0;
+  right: 0;
+  bottom: ${({ mainMenufixed }) => (mainMenufixed ? 'unset' : '0')};
+  z-index: 100;
+`;
+
+export const ContainerNav = styled(Wrapper)`
+  padding: 0 2.5rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 5rem;
+  a {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0 0.9375rem;
+    text-transform: none;
+    text-decoration: none;
+    font-weight: 700;
+    height: 4.0625rem;
+    width: 100%;
+  }
+
+  @media screen and (min-width: 960px) {
+    .container-nav {
+      padding: 0;
+    }
+  }
+`;
+
+interface IMenuProps {
+  menuTop: boolean;
+  displayMenu: boolean;
+}
+
+export const Menu = styled.nav<IMenuProps>`
+  position: absolute;
+  top: ${({ menuTop }) => (menuTop ? '5rem' : '-25.6rem')};
+  left: 0;
+  background-color: ${({ theme }) => theme.colors.mainBlue};
+  width: 100%;
+  border-bottom: 0.5px solid ${({ theme }) => theme.colors.greyBorder};
+  border-top: 0.5px solid ${({ theme }) => theme.colors.greyBorder};
+  display: ${({ displayMenu }) => (displayMenu ? 'block' : 'none')};
+  a {
+    color: ${({ theme }) => theme.colors.greyOne};
+    transition: 0.2s ease-in-out;
+    cursor: pointer;
+    &:hover {
+      color: ${({ theme }) => theme.colors.greyTwo};
+    }
+  }
+
+  @media screen and (min-width: 640px) {
+    top: -8.2rem;
+  }
+
+  @media screen and (min-width: 960px) {
+    display: flex;
+    justify-content: center;
+    border: none;
+    position: unset;
+    top: unset;
+  }
+`;
+
+export const ListMenu = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  li {
+    width: 100%;
+    &:focus {
+      outline-style: none;
+      box-shadow: none;
+      border-color: transparent;
+    }
+    &:focus-visible {
+      a {
+        color: ${({ theme }) => theme.colors.white};
+      }
+    }
+  }
+
+  @media screen and (min-width: 640px) {
+    flex-direction: row;
+    justify-content: center;
+    li {
+      width: auto;
+    }
+  }
+`;
+
+export const SocialNavContainer = styled.div`
+  padding-bottom: 1.25rem;
+  ul {
+    display: flex;
+    justify-content: center;
+  }
+
+  @media screen and (min-width: 640px) {
+    padding: 0;
+  }
+
+  @media screen and (min-width: 960px) {
+    ul {
+      justify-content: unset;
+    }
+  }
+`;
+
+export const BurgerMenu = styled.div`
+  svg {
+    height: 1.875rem;
+    path {
+      fill: ${({ theme }) => theme.colors.greyOne};
+      transition: 0.2s ease-in-out;
+    }
+    &:hover {
+      path {
+        fill: ${({ theme }) => theme.colors.greyTwo};
+      }
+    }
+  }
+`;
---
+++ src/Components/Nav/Nav.tsx
@@ -0,0 +1,149 @@
+import { useState, useEffect, useRef, useCallback, useContext } from 'react';
+import { navContext } from './../../Pages/HomePage';
+import { Link } from 'react-scroll';
+import Logo from './../Logo';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { MainMenu, ContainerNav, Menu, ListMenu, SocialNavContainer, BurgerMenu } from './Nav.styles';
+
+interface INavProps {
+  liList: {
+    name: string;
+    to: string;
+    offset: number;
+    duration: number;
+  }[];
+  navLeftInteraction: JSX.Element;
+}
+
+const Nav = ({ liList, navLeftInteraction }: INavProps) => {
+  const { headerRef } = useContext(navContext);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [menuTop, setMenuTop] = useState(false);
+  const [mainMenufixed, setMainMenuFixed] = useState(false);
+  const [burgerMenuSwitch, setBurgerMenuSwitch] = useState(false);
+  const mainMenu = useRef<HTMLDivElement>(null!);
+  const menu = useRef<HTMLElement>(null!);
+
+  const handleMenuScroll = useCallback(() => {
+    let windowHeight: number = 0;
+    if (headerRef !== null) {
+      if (burgerMenuSwitch && !mainMenufixed) {
+        windowHeight = headerRef.current.scrollHeight - menu.current.scrollHeight - (mainMenu.current.scrollHeight + 1 - menu.current.scrollHeight);
+      } else if (mainMenufixed && burgerMenuSwitch) {
+        windowHeight = headerRef.current.scrollHeight - (mainMenu.current.scrollHeight + 1 - menu.current.scrollHeight);
+      } else {
+        windowHeight = headerRef.current.scrollHeight - (mainMenu.current.scrollHeight + 1);
+      }
+    }
+    let scroll: number = Math.round(window.scrollY);
+
+    if (scroll >= windowHeight) {
+      setMainMenuFixed(true);
+    } else {
+      setMainMenuFixed(false);
+    }
+  }, [headerRef, burgerMenuSwitch]);
+
+  const handleMenuPosition = useCallback(() => {
+    if (windowWidth < 960) {
+      if (menu.current.getBoundingClientRect().top < 0) {
+        setMenuTop(true);
+      } else if (menu.current.getBoundingClientRect().top >= menu.current.offsetHeight + mainMenu.current.offsetHeight) {
+        setMenuTop(false);
+      }
+    } else {
+      if (menu.current.hasAttribute('style')) {
+        setMenuTop(false);
+      }
+      if (setBurgerMenuSwitch) {
+        setBurgerMenuSwitch(false);
+      }
+    }
+  }, [windowWidth, setBurgerMenuSwitch]);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleMenuScroll);
+    window.addEventListener('scroll', handleMenuPosition);
+    return () => {
+      window.removeEventListener('scroll', handleMenuScroll);
+      window.removeEventListener('scroll', handleMenuPosition);
+    };
+  }, [handleMenuScroll, handleMenuPosition]);
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const burgerMenu = (): void => {
+    setBurgerMenuSwitch(!burgerMenuSwitch);
+
+    if (menu.current.getBoundingClientRect().top < 0) {
+      setMenuTop(true);
+    } else if (menu.current.getBoundingClientRect().top >= menu.current.offsetHeight + mainMenu.current.offsetHeight) {
+      setMenuTop(false);
+    }
+  };
+
+  const focusOnKeypress = (elem: string) => {
+    document.getElementById(elem)!.scrollIntoView();
+  };
+
+  let li = liList.map((item, index) => {
+    return (
+      <li
+        tabIndex={0}
+        onKeyPress={() => {
+          focusOnKeypress(item.to);
+        }}
+        key={'nav' + index}
+      >
+        {windowWidth >= 960 && (
+          <Link activeClass="active" to={item.to} spy={true} smooth={true} offset={item.offset} duration={item.duration}>
+            {item.name}
+          </Link>
+        )}
+        {windowWidth < 960 && (
+          <Link activeClass="active" to={item.to} spy={true} smooth={true} offset={item.offset} duration={item.duration} onClick={burgerMenu}>
+            {item.name}
+          </Link>
+        )}
+      </li>
+    );
+  });
+
+  return (
+    <MainMenu ref={mainMenu} mainMenufixed={mainMenufixed}>
+      <ContainerNav>
+        <Logo />
+        <Menu ref={menu} menuTop={menuTop} displayMenu={burgerMenuSwitch}>
+          <ListMenu>{li}</ListMenu>
+          {windowWidth < 960 && <SocialNavContainer>{navLeftInteraction}</SocialNavContainer>}
+        </Menu>
+        {windowWidth >= 960 && <SocialNavContainer>{navLeftInteraction}</SocialNavContainer>}
+        {windowWidth < 960 && (
+          <>
+            {!burgerMenuSwitch && (
+              <BurgerMenu id="burger-svg" onClick={burgerMenu}>
+                <FontAwesomeIcon icon="bars" />
+              </BurgerMenu>
+            )}
+            {burgerMenuSwitch && (
+              <BurgerMenu id="delete-svg" onClick={burgerMenu}>
+                <FontAwesomeIcon icon="times" />
+              </BurgerMenu>
+            )}
+          </>
+        )}
+      </ContainerNav>
+    </MainMenu>
+  );
+};
+
+export default Nav;
