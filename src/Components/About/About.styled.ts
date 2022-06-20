f239a48e06425051e0a8a15fb99ad361d659489e
SHergibo <sachau888@hotmail.com>
2022-06-20 16:27:09 +0200

    feat: Created About component and About.styled file in About folder.

---
+++ src/Components/About/About.styled.ts
@@ -0,0 +1,162 @@
+import styled, { css } from 'styled-components';
+import { Wrapper } from '../../styles/components/Layout.styled';
+import { TitleRigth } from '../../styles/components/Title.styled';
+
+export const AboutStyled = styled.div`
+  overflow: hidden;
+  border-bottom: 0.5px solid ${({ theme }) => theme.colors.greyBorder};
+`;
+
+export const AboutContainer = styled(Wrapper)`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  position: relative;
+  margin: 0 0.9375rem;
+
+  @media screen and (min-width: 640px) {
+    flex-direction: row;
+    margin: 0 auto;
+  }
+`;
+
+interface IImgAboutProps {
+  imgStyled: boolean;
+}
+
+export const ImgAbout = styled.div<IImgAboutProps>`
+  ${({ imgStyled }) =>
+    !imgStyled &&
+    css`
+      display: flex;
+      justify-content: center;
+      overflow: hidden;
+      img {
+        object-fit: contain;
+        max-width: 50%;
+        max-height: 50%;
+        width: auto;
+        height: auto;
+      }
+    `}
+
+  @media screen and (min-width: 640px) {
+    position: relative;
+    width: 25%;
+    background-color: ${({ imgStyled, theme }) => (imgStyled ? 'transparent' : `${theme.colors.btnBlueOver}`)};
+    overflow: hidden;
+    &:after {
+      content: '';
+      position: absolute;
+      background: ${({ theme }) => theme.colors.btnBlueOver};
+      bottom: 0;
+      top: 0;
+      right: 0;
+      width: 1.25rem;
+      z-index: 12;
+    }
+    img {
+      position: absolute;
+      width: 100%;
+      border-bottom: ${({ imgStyled, theme }) => (imgStyled ? `1.25rem solid ${theme.colors.btnBlueOver}` : 'none')};
+    }
+  }
+`;
+
+export const AboutText = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+
+  @media screen and (min-width: 640px) {
+    width: 75%;
+  }
+`;
+
+export const AboutTitle = styled(TitleRigth)`
+  border-top: 0.5px solid ${({ theme }) => theme.colors.greyBorder};
+  &:after,
+  &:before {
+    content: '';
+    position: absolute;
+    background-color: ${({ theme }) => theme.colors.greyBorder};
+    width: 0.9375rem;
+    height: 0.0313rem;
+    top: 0;
+  }
+  &:after {
+    right: -0.9375rem;
+  }
+  &:before {
+    left: -0.9375rem;
+  }
+
+  @media screen and (min-width: 640px) {
+    border-top: none;
+    &:after,
+    &:before {
+      content: unset;
+    }
+  }
+`;
+
+export const AboutMe = styled.div`
+  padding: 3.125rem 1.25rem;
+  h2 {
+    color: ${({ theme }) => theme.colors.white};
+    font-size: 1.875rem;
+  }
+  p {
+    color: ${({ theme }) => theme.colors.greyOne};
+    margin: 0.625rem 0;
+  }
+
+  @media screen and (min-width: 640px) {
+    padding: 1.25rem 1.25rem;
+  }
+`;
+
+export const Info = styled.div`
+  background-color: hsla(0, 0%, 100%, 0.02);
+  position: relative;
+  ul {
+    padding: 1.25rem 1.25rem;
+    li {
+      display: flex;
+      align-items: center;
+      margin: 0.625rem 0;
+      color: ${({ theme }) => theme.colors.greyThree};
+      font-size: 0.8125rem;
+      div {
+        width: 70%;
+      }
+    }
+  }
+  span {
+    display: inline-block;
+    color: ${({ theme }) => theme.colors.white};
+    width: 30%;
+  }
+  &:after {
+    content: '';
+    top: 0;
+    position: absolute;
+    background: inherit;
+    bottom: 0;
+    right: -75rem;
+    width: 75rem;
+  }
+
+  @media screen and (min-width: 640px) {
+    span {
+      width: 8.75rem;
+    }
+  }
+
+  @media screen and (min-width: 960px) {
+    ul {
+      padding: 1.25rem 2.5rem;
+    }
+  }
+`;
---
+++ src/Components/About/About.tsx
@@ -0,0 +1,103 @@
+import { useState, useRef, useEffect, Dispatch, SetStateAction } from 'react';
+import { useRequest } from '../../Hooks/useRequestHooks';
+import useIsLoaded from '../../Hooks/useIsLoadedHook';
+import { apiDomain, apiVersion } from '../../config/environment.config';
+import { IGeneralInfo } from '../../interfaces';
+import { IFetchLoaded } from '../../Pages/HomePage';
+import { AboutStyled, AboutContainer, ImgAbout, AboutText, AboutTitle, AboutMe, Info } from './About.styled';
+import AboutJob from './../AboutJob';
+
+interface IAboutProps {
+  setGeneralInfo: Dispatch<SetStateAction<IGeneralInfo | null>>;
+  setFetchLoaded: Dispatch<SetStateAction<IFetchLoaded>>;
+}
+
+const About = ({ setGeneralInfo, setFetchLoaded }: IAboutProps) => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [imgStyled, setImgStyled] = useState(false);
+  const imgAboutRef = useRef<HTMLDivElement>(null!);
+  const profilePictureRef = useRef<HTMLImageElement>(null!);
+  const { data, error, loading } = useRequest<IGeneralInfo>({
+    method: 'get',
+    url: `${apiDomain}/api/${apiVersion}/infos`
+  });
+
+  useIsLoaded({ loading, error, setStateFunc: setFetchLoaded, objectKey: 'info' });
+
+  useEffect(() => {
+    if (data && !error) setGeneralInfo(data);
+  }, [data, error, setGeneralInfo]);
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (windowWidth >= 640) {
+      if (imgAboutRef.current.offsetHeight - profilePictureRef.current.height >= 20) {
+        setImgStyled(true);
+      }
+    }
+    if (windowWidth < 640) {
+      setImgStyled(false);
+    }
+  }, [windowWidth]);
+
+  return (
+    <AboutStyled id="about">
+      <AboutContainer>
+        <ImgAbout ref={imgAboutRef} imgStyled={imgStyled}>
+          {data?.profilePic?.fileName && <img ref={profilePictureRef} src={`${apiDomain}/api/${apiVersion}/infos/image/${data?.profilePic?.fileName}`} alt={data?.profilePic?.alt} />}
+          {!data?.profilePic?.fileName && <img ref={profilePictureRef} src="./default-profile-picture.png" alt="Profil par défaut" />}
+        </ImgAbout>
+        <AboutText>
+          <AboutTitle>À propos</AboutTitle>
+          <AboutMe>
+            <h2>Bonjour !</h2>
+            <p>{data?.description}</p>
+          </AboutMe>
+          <Info>
+            <ul>
+              <li>
+                <span>Téléphone :</span>
+                <div>{data?.phone}</div>
+              </li>
+              <li>
+                <span>Email :</span>
+                <div>{data?.email}</div>
+              </li>
+              <li>
+                <span>Adresse :</span>
+                <div>
+                  {data?.address.street && (
+                    <>
+                      {data?.address.street}, {data?.address.number} {data?.address.zip} {data?.address.city}
+                    </>
+                  )}
+                </div>
+              </li>
+              <li>
+                <span>Date de naissance :</span>
+                <div>{data?.birthdate}</div>
+              </li>
+              <li>
+                <span>Permis de conduire :</span>
+                <div>{data?.licence}</div>
+              </li>
+            </ul>
+          </Info>
+        </AboutText>
+      </AboutContainer>
+      <AboutJob generalInfo={data} />
+    </AboutStyled>
+  );
+};
+
+export default About;
