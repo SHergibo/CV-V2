85ca6e9ad88b797328b1eb46d0dd89f2b9b6c944
SHergibo <sachau888@hotmail.com>
2022-06-21 14:37:23 +0200

    feat: Added css border in styled component Theme file.

--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -1,37 +1,47 @@
 import { DefaultTheme } from 'styled-components';
 import 'styled-components';
 
+interface IColorsProps {
+  mainBlue: string;
+  blue: string;
+  btnBlueOver: string;
+  white: string;
+  greyOne: string;
+  greyTwo: string;
+  greyThree: string;
+  greyBorder: string;
+}
+
 declare module 'styled-components' {
   export interface DefaultTheme {
-    colors: {
-      mainBlue: string;
-      blue: string;
-      btnBlueOver: string;
-      white: string;
-      greyOne: string;
-      greyTwo: string;
-      greyThree: string;
-      greyBorder: string;
-    };
+    colors: IColorsProps;
     font: {
       principalFont: string;
+    };
+    css: {
+      border: string;
     };
   }
 }
 
+const colors: IColorsProps = {
+  mainBlue: 'hsl(211, 80%, 10%)',
+  blue: 'hsl(208, 100%, 43%)',
+  btnBlueOver: 'hsl(232, 68%, 43%)',
+  white: '#ffffff',
+  greyOne: 'hsla(0, 0%, 100%, 0.7)',
+  greyTwo: 'hsla(0, 0%, 100%, 0.5)',
+  greyThree: 'hsla(0, 0%, 100%, 0.62)',
+  greyBorder: 'hsla(0, 0%, 100%, 0.3)'
+};
+
 const theme: DefaultTheme = {
-  colors: {
-    mainBlue: 'hsl(211, 80%, 10%)',
-    blue: 'hsl(208, 100%, 43%)',
-    btnBlueOver: 'hsl(232, 68%, 43%)',
-    white: '#ffffff',
-    greyOne: 'hsla(0, 0%, 100%, 0.7)',
-    greyTwo: 'hsla(0, 0%, 100%, 0.5)',
-    greyThree: 'hsla(0, 0%, 100%, 0.62)',
-    greyBorder: 'hsla(0, 0%, 100%, 0.3)'
-  },
+  colors,
   font: {
     principalFont: '"Poppins", sans-serif'
+  },
+  css: {
+    border: `0.5px solid ${colors.greyBorder};`
   }
 };
 
