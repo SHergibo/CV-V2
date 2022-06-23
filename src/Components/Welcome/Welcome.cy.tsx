4b7d427606e1e5d38e13673da820c7e112a7e269
SHergibo <sachau888@hotmail.com>
2022-06-23 14:35:49 +0200

    feat: Added multiple tests in Welcome.cy.tsx cypress test file.

--- src/Components/Welcome/Welcome.cy.tsx
+++ src/Components/Welcome/Welcome.cy.tsx
@@ -3,8 +3,8 @@ import { open } from '../../CypressUtils/cypressTest-open';
 import CypressMount from '../../CypressUtils/CypressMount';
 import { MemoryRouter, Routes, Route } from 'react-router-dom';
 
-describe('<Welcome>', () => {
-  it('mounts', () => {
+describe('<Welcome> and <WelcomeSection> Testing', () => {
+  it('Testing for Welcome when url is "/"', () => {
     cy.fixture('generalInfo').then((generalInfo) => {
       open(
         <CypressMount>
@@ -16,5 +16,67 @@ describe('<Welcome>', () => {
         </CypressMount>
       );
     });
+    cy.findByText('Bienvenue').should('exist');
+    cy.findByText('Sacha Hergibo').should('exist');
+    cy.findByText('Test6').should('exist');
+    cy.findByText('Test').should('exist');
+  });
+  it('Testing for Welcome when generalInfo has no profession title', () => {
+    cy.fixture('generalInfoWithoutProfTitle').then((generalInfo) => {
+      open(
+        <CypressMount>
+          <MemoryRouter initialEntries={['/']}>
+            <Routes>
+              <Route path="/" element={<Welcome generalInfo={generalInfo} />} />
+            </Routes>
+          </MemoryRouter>
+        </CypressMount>
+      );
+    });
+    cy.findByText('Bienvenue').should('exist');
+    cy.findByText('Sacha Hergibo').should('exist');
+    cy.findByText('Je suis un').should('not.exist');
+  });
+  it('Testing for Welcome when url is "/admin"', () => {
+    cy.fixture('generalInfo').then((generalInfo) => {
+      open(
+        <CypressMount>
+          <MemoryRouter initialEntries={['/admin']}>
+            <Routes>
+              <Route path="/admin" element={<Welcome generalInfo={generalInfo} />} />
+            </Routes>
+          </MemoryRouter>
+        </CypressMount>
+      );
+    });
+    cy.findAllByText('Administration').should('have.length', 2);
+    cy.findByText('Sacha Hergibo').should('exist');
+  });
+  it('Testing for Welcome without generalInfo prop when url is "/"', () => {
+    open(
+      <CypressMount>
+        <MemoryRouter initialEntries={['/']}>
+          <Routes>
+            <Route path="/" element={<Welcome generalInfo={null} />} />
+          </Routes>
+        </MemoryRouter>
+      </CypressMount>
+    );
+    cy.findByText('Bienvenue').should('exist');
+    cy.findByText('Mon site CV').should('exist');
+  });
+  it('Testing for Welcome without generalInfo prop when url is "/admin"', () => {
+    open(
+      <CypressMount>
+        <MemoryRouter initialEntries={['/admin']}>
+          <Routes>
+            <Route path="/admin" element={<Welcome generalInfo={null} />} />
+          </Routes>
+        </MemoryRouter>
+      </CypressMount>
+    );
+
+    cy.findAllByText('Administration').should('have.length', 2);
+    cy.findByText('Mon site CV').should('exist');
   });
 });
