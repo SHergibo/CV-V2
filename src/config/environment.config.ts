76d753c22a6dc53b2e6d251c439833797177d436
SHergibo <sachau888@hotmail.com>
2022-06-27 19:19:23 +0200

    feat: Updated environment.config to add Cypress environment variable when testing with Cypress.

--- src/config/environment.config.ts
+++ src/config/environment.config.ts
@@ -1,4 +1,4 @@
 const { REACT_APP_API_DOMAIN, REACT_APP_API_VERSION } = process.env;
 
-export const apiDomain: string | undefined = REACT_APP_API_DOMAIN;
-export const apiVersion: string | undefined = REACT_APP_API_VERSION;
+export const apiDomain: string | undefined = REACT_APP_API_DOMAIN || Cypress.env('api_domain');
+export const apiVersion: string | undefined = REACT_APP_API_VERSION || Cypress.env('api_version');
